b1e4a9ab2787b73bd659601262fd248539bea260
Satvik Jayalwal <satvikjayalwalips@gmail.com>
2026-09-12 16:58:52 +0530

    Implemented global PyTest hook in conftest.py for automated screenshot capture on failure

--- conftest.py
+++ conftest.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import pytest
 from configparser import ConfigParser
+import os
+from datetime import datetime
 
 chrome_options = Options()
 # chrome_options.add_argument("--headless=new")
@@ -22,4 +24,20 @@ def driver():
     driver.get(BASE_URL)    
     yield driver    
     driver.quit()
-        
+
+@pytest.hookimpl(tryfirst=True,hookwrapper=True)
+def pytest_runtest_makereport(item,call):
+    outcome = yield
+    rep = outcome.get_result()
+
+    if rep.when == "call" and rep.failed:
+        driver = item.funcargs.get("driver",None)       
+
+        if driver:
+
+            os.makedirs("screenshots",exist_ok=True)
+            timestamp = datetime.now().strftime("%H-%M-%S")
+            file_path = f"screenshots/{item.name}_{timestamp}.png"
+
+            driver.save_screenshot(file_path)
+            print(f"\nSCREENSHOT SAVED AT: {file_path}")
---
+++ tests/test_failure.py
@@ -0,0 +1,24 @@
+from pages.admin_login_page import AdminLoginPage
+
+def test_login_with_admin(driver):
+    admin_login = AdminLoginPage()
+
+    print("\nCLICKING ON ADMIN LOGIN")
+    admin_login.open_admin_login_page()
+
+    print("\nVERIFYING ADMIN LOGIN TITLE IS VISIBLE")
+    assert admin_login.is_admin_login_title_displayed(),"ADMIN LOGIN TITLE NOT DISPLAYED"
+    print("ADMIN LOGIN TITLE DISPLAYED SUCCESSFULLY")
+
+    print("\nFILLING USERNAME")
+    admin_login.fill_admin_login_username()
+
+    print("\nFILLING PASSWORD")
+    admin_login.fill_admin_login_password()
+
+    print("\nCLICKING LOGIN BUTTON")
+    admin_login.click_admin_login_btn
+
+    print("\nVERIFYING ADMIN DASHBOARD TITLE IS VISIBLE")
+    assert admin_login.is_admin_dashboard_displayed, "\nADMIN DASHBOARD TITLE NOT DISPLAYED"
+    print("\nADMIN DASHBOARD TITLE DISPLAYED SUCCESSFULLY")
